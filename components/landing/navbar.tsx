dacaf2b7f8c2a8c2b9caa93fec5aad511eb04871
Mustafa Khaled <mostafakhaled0787314@gmail.com>
2025-12-13 21:29:26 +0200

    update: logo

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export function Navbar() {
   return (
     <nav className="fixed top-0 w-full z-50 border-b border-white/5 bg-background/60 backdrop-blur-xl supports-backdrop-filter:bg-background/60">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <div className="size-10 rounded-xl bg-linear-to-tr from-white/20 to-white/5 p-2 backdrop-blur-sm border border-white/10 shadow-lg mb-1">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
+          <div className="size-12 rounded-full bg-linear-to-tr from-white/20 to-white/5 p-1 backdrop-blur-sm border border-white/10 shadow-lg mb-1">
+            <Image
               src="/logo.png"
               alt="Novus Logo"
-              className="w-full h-full object-contain"
+              className="w-full h-full object-contain rounded-full"
+              width={40}
+              height={40}
             />
           </div>
           <span className="font-bold text-2xl tracking-tighter text-foreground">
